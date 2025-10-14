2a5025062c17b70a5dde70b734142cac6db486b7
Dsecion <161196964+Dsecion@users.noreply.github.com>
2025-10-14 20:37:29 +0800

    初始提交

---
+++ 移植 - 全项目/Objects/usart.d
@@ -0,0 +1,4 @@
+.\objects\usart.o: hardware\usart.c
+.\objects\usart.o: hardware\usart.h
+.\objects\usart.o: D:\Keil_v5\ARM\compile\Bin\..\include\stdio.h
+.\objects\usart.o: D:\Keil_v5\ARM\compile\Bin\..\include\stdint.h
